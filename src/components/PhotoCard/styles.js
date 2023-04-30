b4374afcc1c3ec2d85126dda0cf58014d0a530db
Andres Maneiro <amaneiro7@gmail.com>
2023-04-29 22:37:23 -0400

    skeleton animation

--- src/components/PhotoCard/styles.js
+++ src/components/PhotoCard/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { fadeIn } from '@/styles/animation'
+import { loadingSkeleton } from '@/styles/loadingSkeleton'
 
 export const ImgWrapper = styled.div`
     border-radius: 10px;
@@ -13,9 +14,10 @@ export const ImgWrapper = styled.div`
 export const Img = styled.img`
     ${fadeIn()}
     box-shadow: 0 10px 14px rgba(0, 0, 0, .2);
-    height: 100%;
     object-fit: cover;    
     width: 100%;
+    height: 100%;
+    ${loadingSkeleton()}
 `
 
 export const Button = styled.button`
