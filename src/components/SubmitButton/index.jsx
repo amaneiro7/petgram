ee16db33d8bdc6c8d7fe310094923dc03bec7805
Andres Maneiro <amaneiro7@gmail.com>
2023-04-30 22:43:50 -0400

    estilos del button

---
+++ src/components/SubmitButton/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Button } from './style'
+
+export const SubmitButton = ({ children, onClick, disabled }) => {
+  return (
+    <Button onClick={onClick} disabled={disabled}>{children}</Button>
+  )
+}
---
+++ src/components/SubmitButton/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    display: block;
+    width: 100%;
+    height: 32px;
+    background: #8d00ff;
+    color: #fff;
+    text-align: center;
+    border-radius: 3px;
+    cursor: pointer;
+    &[disabled] {
+        opacity: .3;
+    }
+`
