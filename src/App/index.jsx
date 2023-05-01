a8fdb8a78d81a70864be37852ad238d7060ee27b
Andres Maneiro <amaneiro7@gmail.com>
2023-05-01 00:10:32 -0400

    implementando lazy import

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,15 +1,21 @@
+import { Suspense, lazy } from 'react'
 import ErrorBoundary from './ErrorBoundary'
 import { BrowserRouter } from 'react-router-dom'
-import { Layout } from '@/components/Layout'
-import { PublicRoute } from '@/routes'
+// import { Layout } from '@/components/Layout'
+// import { PublicRoute } from '@/routes'
+
+const Layout = lazy(() => import('@/components/Layout').then(module => ({ default: module.Layout })))
+const PublicRoute = lazy(() => import('@/routes').then(module => ({ default: module.PublicRoute })))
 
 function App () {
   return (
     <ErrorBoundary>
       <BrowserRouter>
-        <Layout>
-          <PublicRoute />
-        </Layout>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Layout>
+            <PublicRoute />
+          </Layout>
+        </Suspense>
       </BrowserRouter>
     </ErrorBoundary>
   )
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,19 +1,20 @@
-import React, { useContext } from 'react'
+import React, { Suspense, lazy, useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home } from '@/pages/Home'
-import { Detail } from '@/pages/Detail'
-import { NotFound } from '@/pages/NotFound'
 import { AppContext } from '@/context'
 import { authenticatedRoute, protectedRoute } from './PrivateRoute'
 
-export function PublicRoute () {
+const Home = lazy(() => import('@/pages/Home').then(module => ({ default: module.Home })))
+const Detail = lazy(() => import('@/pages/Detail').then(module => ({ default: module.Detail })))
+const NotFound = lazy(() => import('@/pages/NotFound').then(module => ({ default: module.NotFound })))
+
+export const PublicRoute = () => {
   const { isAuth } = useContext(AppContext)
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/pet/:id' element={<Home />} />
-        <Route path='/detail/:detailId' element={<Detail />} />
+        <Route path='/' element={<Suspense fallback={<p>Loading...</p>}><Home /></Suspense>} />
+        <Route path='/pet/:id' element={<Suspense fallback={<p>Loading...</p>}><Home /></Suspense>} />
+        <Route path='/detail/:detailId' element={<Suspense fallback={<p>Loading...</p>}><Detail /></Suspense>} />
         {isAuth
           ? authenticatedRoute()
           : protectedRoute()}
