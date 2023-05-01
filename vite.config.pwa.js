da8aa76b5c15c5cbc13e328e3a2351375c824616
Andres Maneiro <amaneiro7@gmail.com>
2023-05-01 03:22:26 -0400

    change vite config

--- vite.config.pwa.js
+++ vite.config.pwa.js
@@ -5,6 +5,9 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    manifest: true
+  },
   resolve: {
     alias: [{
       find: '@', replacement: path.resolve(__dirname, 'src')
